03e55447c605f6a1607d199ba98059b8d410450d
Ricardo Mello <ricardo.mello@outlook.com>
2016-09-25 19:45:57 -0300

    Projeto criado com Spring Initializr: https://start.spring.io/

---
+++ src/main/java/com/ricardo/mello/HoteisApplication.java
@@ -0,0 +1,12 @@
+package com.ricardo.mello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteisApplication.class, args);
+	}
+}
