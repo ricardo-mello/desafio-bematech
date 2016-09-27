df145c151610e1f374da5e006b8cc44b4c5b17b2
Ricardo Mello <ricardo.mello@outlook.com>
2016-09-26 23:32:57 -0300

    Controlador RESTful para API de Hotéis

--- src/main/java/com/ricardo/mello/repository/HotelRepository.java
+++ src/main/java/com/ricardo/mello/repository/HotelRepository.java
@@ -1,10 +1,7 @@
 package com.ricardo.mello.repository;
 
 import com.ricardo.mello.bean.Hotel;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 /**
  * Repositório para a entidade Hotel.
